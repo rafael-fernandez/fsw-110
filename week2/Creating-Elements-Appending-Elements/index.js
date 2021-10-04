5d6d4e3a75fcfc376521826a0ef43dbd8ada0dd3
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2021-10-04 16:10:05 -0600

    Added index.html and index.js to week2 Creating & Appending Elements

---
+++ week2/Creating-Elements-Appending-Elements/index.js
@@ -0,0 +1,18 @@
+
+var hello = document.getElementById("welcome");
+welcome.textContent = "Welcome to my JS site";
+
+var created = document.getElementById("created");
+created.textContent = "All of this was created with Javascript";
+
+
+document.getElementById("firstName")
+firstName.textContent = "First Name:";
+
+document.getElementById("lastName")
+lastName.textContent = "Last Name:";
+
+document.getElementById("theAge")
+theAge.textContent = "Age:";
+
+
