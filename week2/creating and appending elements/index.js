0ceb74ddee8704e41adbc8e68e7c0485935335e4
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2021-10-05 20:55:11 -0600

    modified index.js

--- week2/creating and appending elements/index.js
+++ week2/creating and appending elements/index.js
@@ -1,4 +1,59 @@
 
+var h1 = document.createElement("h1");
+h1.innerText = "Welcome to my JS site"
+document.body.appendChild(h1);
+
+var p = document.createElement("p");
+p.innerText = "All of this was created with Javascript";
+document.body.appendChild(p);
+
+
+
+var first = document.createElement("firstName")
+first.textContent = "1. First Name: ";
+document.body.appendChild(first);
+
+var second = document.createElement("lastName")
+second.textContent = "2. Last Name: ";
+document.body.appendChild(second);
+
+var third = document.createElement("theAge")
+third.textContent = "3. Age:";
+document.body.appendChild(third);
+
+
+
+
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 var hello = document.getElementById("welcome");
 welcome.textContent = "Welcome to my JS site";
 
@@ -15,4 +70,5 @@ lastName.textContent = "Last Name:";
 document.getElementById("theAge")
 theAge.textContent = "Age:";
 
-
+})
+*/
