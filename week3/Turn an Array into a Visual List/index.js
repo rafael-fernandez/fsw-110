ae645a222c179d6552d444d831ca98a37b763b55
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2021-10-12 18:29:26 -0600

    week3 Turn an Array into a Visual List index.html and index.js

---
+++ week3/Turn an Array into a Visual List/index.js
@@ -0,0 +1,89 @@
+/* 2) Create an H1 tag with the text “Hello World”, and append it to the DOM (test and make sure it is showing up on the website). */
+
+/* #3 Create a for-loop, repeat this process 10 times so that 10 H1’s with the text “Hello World” show up when the website is visited. */
+
+for (let i = 0; i < 10; i++) {
+    var repeat = document.createElement("h1");
+    repeat.textContent = "Hello World!";
+    document.body.append(repeat)
+    repeat.style.color = 'blue'
+    repeat.style.textDecoration = 'underline'
+
+}
+
+/* 4) Add some style to the H1's in Step 3. */
+var span = document.getElementsByTagName("h1")[1];
+span.style.color = "red";
+
+var span = document.getElementsByTagName("h1")[3];
+span.style.color = "red";
+
+var span = document.getElementsByTagName("h1")[5];
+span.style.color = "red";
+
+var span = document.getElementsByTagName("h1")[7];
+span.style.color = "red";
+
+var span = document.getElementsByTagName("h1")[9];
+span.style.color = "red";
+
+
+/* 5) Create a new array using the following information: */
+
+var names = [
+    "Steve",
+    "Larry",
+    "Joe",
+    "Shirley",
+    "Steph",
+    "Nate",
+    "Rick",
+    "Emily"
+
+];
+
+/* Create a for loop that iterates through the array in Step 5 and displays it on the page */
+for (var i = 0; i < names.length; i++) {
+    document.write("<li>" + names[i] + "</li>");
+    //style.textDecoration = 'underline'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var powerRangers = [
+    "Jason Lee Scott",
+    "Kimberly Hart",
+    "Zack Taylor",
+    "Trini Kwan",
+    "Billy Cranston",
+    "Patrick Morgan"
+]
+
+// we want to select these array items, create an UL, and place these array items as children list items
+
+var rangersList = document.getElementById("rangers")
+
+for (var i = 0; i < powerRangers.length; i++) {
+
+    var myFriends = document.createElement("friends");
+    myFriends.textContent = powerRangers[i];
+
+}
+
+*/
