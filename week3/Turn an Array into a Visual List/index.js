d214d8ab8eb9dc43f3dcc8f2a9975ce37dbe3362
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2021-10-12 18:59:30 -0600

    added screenshot

--- week3/Turn an Array into a Visual List/index.js
+++ week3/Turn an Array into a Visual List/index.js
@@ -1,3 +1,6 @@
+/* 2) Create an H1 tag with the text “Hello World”, and append it to the DOM (test and make sure it is showing up on the website). */
+
+/*3) Create a for-loop, repeat this process 10 times so that 10 H1’s with the text “Hello World” show up when the website is visited. */
 for (let i = 0; i < 10; i++) {
     var repeat = document.createElement("h1");
     repeat.textContent = "Hello World!";
@@ -8,7 +11,7 @@ for (let i = 0; i < 10; i++) {
 
 }
 
-
+/* 4) Add some style to the H1's in Step 3. */
 var span = document.getElementsByTagName("h1")[1];
 span.style.color = "red";
 
@@ -25,8 +28,7 @@ var span = document.getElementsByTagName("h1")[9];
 span.style.color = "red";
 
 
-
-
+/* 5) Create a new array using the following information: */
 var names = [
     "Steve",
     "Larry",
@@ -38,7 +40,8 @@ var names = [
     "Emily"
 
 ];
-
+ 
+/* 6) Create a for loop that iterates through the array in Step 5 and displays it on the page. */
 for (var i = 0; i < names.length; i++) {
     document.write("<li>" + names[i] + "</li>");
     //style.textDecoration = 'underline'
