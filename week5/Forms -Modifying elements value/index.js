bac5cc4d497065a4923615ac3340add40e314b25
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2021-10-26 20:39:07 -0600

    modified index.html,js and css. Also added png

--- week5/Forms -Modifying elements value/index.js
+++ week5/Forms -Modifying elements value/index.js
@@ -1,6 +1,17 @@
-let btnClear = document.querySelector('button');
-let inputs = document.querySelector('input')
 
-btnClear.addEventListener('click', () => {
-    inputs.forEach(input => input.value = ' ');
-});
+
+function getdetails() {
+    var fName = document.forms["details"]["firstname"].value;
+    var lName = document.forms["details"]["lastname"].value;
+   
+    var email = document.forms["details"]["email"].value;
+    var boxM = document.forms["details"]["message"].value;
+
+    alert("My name is " + fName + " " + lName + ", " + "My email is " + " " + " " + email + ". " + "Here is what I have to say: " + " " + boxM );
+
+    let btnClear = document.querySelector('button');
+    btnClear.addEventListener('click', () => {
+        inputs.forEach(input => input.value = ' ');
+    })
+
+}
