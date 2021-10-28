5ab49deec04b06c0dadf6f662d5e689dcf05dcdc
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2021-10-27 23:15:11 -0600

    modified all files

--- week5/Forms - other types of HTML inputs/index.js
+++ week5/Forms - other types of HTML inputs/index.js
@@ -1,34 +1,35 @@
-const resultsList = document.getElementById('results')
-new URLSearchParams(window.location.search).forEach((value, name) => {
-
-    resultsList.append(`${name}: ${value}`)
-    resultsList.append(document.createElement('br'))
-});
 
 
-function calculate_age() 
-{
-    var birth_date = new Date(document.getElementById("birth_date").value);
-    var birth_date_day = birth_date.getDate();
-    var birth_date_month = birth_date.getMonth();
-    var birth_date_year = birth_date.getFullYear();
 
-    var today_date = new Date();
-    var today_day = today_date.getDate();
-    var today_month = today_date.getMonth();
-    var today_year = today_date.getFullYear();
+function getdetails() {
+    var fName = document.forms["details"]["firstname"].value;
+    var lName = document.forms["details"]["lastname"].value;
+    var age = document.forms["details"]["age"].value;
+    var gender = document.forms["details"]["gender"].value;
+    //var date = document.forms["details"]["date"].value;
+    var email = document.forms["details"]["email"].value;
+    var hear  = document.forms["details"]["hear"].value;
+    var recom = document.forms["details"]["recom"].value;
+    var goodTime = document.forms["details"]["goodTime"].value;
+    var ack = document.forms["details"]["ack"].value;
+    
+  
+    
 
-    var calculated_age = 0;
+    alert("First name: " + fName + " " + "Last name: " + lName + ", " + "Age:" + age + " " + "Gender: " + gender + ". " + "Email: " + email + ". " + "Will you recommend our service?" + " " + "I will" + " " + recom + " " + "recommend your services." + " " + "How did you hear about us?" + " " + hear + ", " + "I will make myself " + " " + goodTime + " " + "during this time slot." + " " + "Account:" + " " + ack );
 
-    if (today_month > birth_date_month) calculated_age = today_year - birth_date_year;
-    else if (today_month == birth_date_month) {
+ 
 
-        if(today_day >= birth_date_day) calculated_age = today_year - birth_date_year;
-        else calculated_age = today_year - birth_date_year - 1;
-    }
-    else calculated_age = today_year - birth_date_year -1;
 
-    var output_value = calculated_age;
-    document.getElementById("calculated_age").textContent = calculated_age;
 
-} 
+    let btnClear = document.querySelector('button');
+    btnClear.addEventListener('click', () => {
+        inputs.forEach(input => input.value = ' ');
+    })
+
+}
+
+ 
+
+
+
