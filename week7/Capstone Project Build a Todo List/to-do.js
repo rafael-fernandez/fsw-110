0409623448dd147028e12041ab2cb90f2b8a1209
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2021-11-15 20:30:04 -0700

    initial commit

---
+++ week7/Capstone Project Build a Todo List/to-do.js
@@ -0,0 +1,52 @@
+
+
+let addToDoButton = document.getElementById('addToDo');
+let main = document.getElementById('main');
+let inputField = document.getElementById('inputField');
+ 
+addToDoButton.addEventListener('click', function(){
+    var toDoContainer =  document.createElement('div');
+    toDoContainer.classList.add('row')
+    var newItem = document.createElement('div');
+    newItem.classList.add('column');
+   
+    var remButton= document.createElement('button');
+       
+    newItem.innerText = inputField.value;
+    
+     remButton.innerText = "delete"
+      toDoContainer.appendChild(newItem);
+       toDoContainer.appendChild(remButton)
+    main.appendChild(toDoContainer)
+     inputField.value = "";
+ 
+    remButton.addEventListener('click', function (e) {
+           main.removeChild(toDoContainer)
+           
+   
+     })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
